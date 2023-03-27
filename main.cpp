f8c0a251dc0569cb20b8e7d2a15c5fecd31cc234
Hersh Kumar <hershkumar13@gmail.com>
2023-03-27 10:46:05 -0400

    get color gradient and spawning working

--- main.cpp
+++ main.cpp
@@ -12,19 +12,26 @@ const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 800;
 
 
-// function that returns a text object for the frame_count
-//TODO: change this to compute the fps
-sf::Text fps(double frame_num, sf::Font font) {
+// compute the fps
+sf::Text fps(std::vector<float> frameTimes, sf::Font font) {
+    int numFramesToAverage = frameTimes.size();
+
+    float sum = 0;
+    for (auto& n : frameTimes)
+        sum += n;
+    // fps is number of frames divided by the time it took to render them
+    float fps = static_cast<float> (numFramesToAverage) / sum;
+
     sf::Text frame_text;
     frame_text.setFont(font);
-    frame_text.setString(std::to_string(frame_num));
+    frame_text.setString(std::to_string(fps));
     frame_text.setCharacterSize(15);
     frame_text.setFillColor(sf::Color::Red);
     frame_text.setStyle(sf::Text::Bold);
     return frame_text;
 }
 
-
+// returns the text that displays the number of drawn circles
 sf::Text printDrawn(int num, sf::Font font){
     sf::Text drawn_text;
     drawn_text.setFont(font);
@@ -36,10 +43,64 @@ sf::Text printDrawn(int num, sf::Font font){
     return drawn_text;
 }
 
+// generates an rgb color gradient
+std::vector<std::vector<int>> generateGradient(int numColors) {
+    std::vector<std::vector<int>> gradient;
+    double hueIncrement = 360.0 / numColors;
+    double saturation = 1.0;
+    double value = 1.0;
+    for (int i = 0; i < numColors; i++) {
+        double hue = i * hueIncrement;
+        double chroma = value * saturation;
+        double x = chroma * (1 - std::abs(fmod(hue / 60, 2) - 1));
+        double r1, g1, b1;
+        if (hue < 60) {
+            r1 = chroma;
+            g1 = x;
+            b1 = 0;
+        } else if (hue < 120) {
+            r1 = x;
+            g1 = chroma;
+            b1 = 0;
+        } else if (hue < 180) {
+            r1 = 0;
+            g1 = chroma;
+            b1 = x;
+        } else if (hue < 240) {
+            r1 = 0;
+            g1 = x;
+            b1 = chroma;
+        } else if (hue < 300) {
+            r1 = x;
+            g1 = 0;
+            b1 = chroma;
+        } else {
+            r1 = chroma;
+            g1 = 0;
+            b1 = x;
+        }
+        double m = value - chroma;
+        int r = (r1 + m) * 255;
+        int g = (g1 + m) * 255;
+        int b = (b1 + m) * 255;
+        gradient.push_back({r, g, b});
+    }
+    return gradient;
+}
+
+std::vector<float> generateRange(float start, float end, int numSteps) {
+    float stepSize = (end - start) / (numSteps - 1);
+    std::vector<float> range(numSteps);
+    for (int i = 0; i < numSteps; i++) {
+        range[i] = start + i * stepSize;
+    }
+    return range;
+}
+
 class Circle {
     public:
         // radius of the circle
-        float radius = 15.f;
+        float radius = 10.f;
         // current position as a vector
         sf::Vector2f pos;
         // old position as a vector
@@ -52,6 +113,7 @@ class Circle {
         sf::Color color = sf::Color::White;
         // whether the circle has been drawn onto the window
         bool drawn;
+
         Circle(){
             shape.setRadius(radius);
             shape.setFillColor(color);
@@ -88,20 +150,18 @@ class Circle {
 };
 
 
-
 class Simulator{
     public:
         // the circles
         std::vector<Circle> circles;
         // the force to be applied
-        sf::Vector2f force = {0, 1.0f};
+        sf::Vector2f force = {0, 3000.0f};
         // number of circles
-        int num_circles = 2000;
+        int num_circles = 300;
         // the time step
-        float dt = 0.01f;
-
+        float dt = 0.001f;
         // bounding circle
-        float bounding_radius = 375.f;
+        float bounding_radius = 400.f;
         // bounding circle bg_color
         sf::Color bounding_color = sf::Color::Black;
         // shape for the bounding circle
@@ -111,9 +171,14 @@ class Simulator{
         // number of currently drawn circles
         int num_drawn = 0;
         // number of substeps
-        int substeps = 1;
+        int substeps = 4;
 
     Simulator() {
+        // get a color gradient 
+        std::vector<std::vector<int>> gradient = generateGradient(50);
+        // get a range of radii
+        std::vector<float> radii = generateRange(10, 10, 50);
+
         // create the circles
         for (int i = 0; i < num_circles; i++){
             Circle circle;
@@ -121,6 +186,15 @@ class Simulator{
             circle.pos = {static_cast<float>(WINDOW_WIDTH/2.0), static_cast<float>(WINDOW_HEIGHT/2.0) - 200.0};
             circle.old_pos = circle.pos;
             circle.drawn = false;
+            // set the color of the circle
+            int colorIndex = i % gradient.size();
+            circle.color = sf::Color(gradient[colorIndex][0], gradient[colorIndex][1], gradient[colorIndex][2]);
+            
+            // set the radius of the circle
+            int radiusIndex = i % radii.size();
+            circle.radius = radii[radiusIndex];
+            circle.shape.setRadius(circle.radius);
+
             circles.push_back(circle);
         }
 
@@ -139,7 +213,6 @@ class Simulator{
         Simulator();
     }
 
-    //TODO: substep this like in the video
     void update(float dt) {
         float sdt = dt / static_cast<float>(substeps);
         for (int i = 0; i < substeps; i++){
@@ -189,6 +262,8 @@ class Simulator{
                         // compute the normal vector along the collision axis
                         sf::Vector2f normal = vec_to_other / dist;
                         // move the two objects till they don't collide anymore
+                        //TODO: this doesn't work for collisions between circles with different radii
+                        //we're shifting them by the same amount, but they should be shifted by different amounts
                         float delta = circles[i].radius + circles[j].radius - dist;
                         circles[i].pos += .5f * delta * normal;
                         circles[j].pos -= .5f * delta * normal;
@@ -196,6 +271,12 @@ class Simulator{
                 }
             }
         }
+    }
+
+    //TODO: start working on this spatial partitioning algorithm
+    // spatial partitioning algorithm for collision detection
+    void checkCollisions2(){
+
     }
 
     void draw(sf::RenderWindow &window){
@@ -225,9 +306,14 @@ int main()
     // make a simulator instance
     Simulator sim;
 
+
+    std::vector<float> frameTimes;
     int frame_num = 1;
     while (window.isOpen())
     {
+        // start the clock for the frame
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
         frame_num++;
 
         // check all the window's events that were triggered since the last iteration of the loop
@@ -239,12 +325,14 @@ int main()
                 window.close();
         }
         // based on the frame number, set the correct number of circles to be drawn
-        if (frame_num % 250 == 0){
+        if (frame_num % 150 == 0){
             if (sim.num_drawn < sim.num_circles){
                 sim.circles[sim.num_drawn].drawn = true;
-                sf::Vector2f spawnForce = {static_cast<float>(sin(sim.num_drawn/(10*2*3.14))),static_cast<float>(cos(sim.num_drawn/(10*2*3.14)))};
-                spawnForce *= 2000.0f;
+                // when we spawn a new circle, give it an impulse 
+                sf::Vector2f spawnForce = {static_cast<float>(sin(sim.num_drawn/(2*3.14))),static_cast<float>(cos(sim.num_drawn/(2*3.14)))};
+                spawnForce *= 10000000.0f;
                 sim.applyForceToSingle(sim.circles[sim.num_drawn], spawnForce);
+
                 sim.num_drawn++;
             }
         }
@@ -258,10 +346,23 @@ int main()
         sim.draw(window);
 
         // display frame number in top left
-        window.draw(fps(frame_num, font));
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        float frameTime = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+        frameTimes.push_back(frameTime);
+        if (frameTimes.size() > 100){
+            frameTimes.erase(frameTimes.begin());
+        }
+        // print the frames per second
+        window.draw(fps(frameTimes, font));
+
+        // print the number of drawn circles
         window.draw(printDrawn(sim.num_drawn, font));
         // end the current frame
         window.display();
+
+        
+
+
     }
 
     return 0;
